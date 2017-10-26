aaa54d8947ffd1b0361614497af297a65c95a151
hillsdej <hillsdej@uni.coventry.ac.uk>
2017-10-26 16:36:16 +0100

    created Items table

--- database.js
+++ database.js
@@ -6,11 +6,26 @@ var con = mysql.createConnection({
   password: "password"
 });
 
-con.connect(function(err) {
-    if (err) throw err;
-    console.log("Connected!");
-    con.query("CREATE DATABASE Stock", function (err, result) {
-      if (err) throw err;
-      console.log("Database created");
+// con.connect(function(err) {
+//     if (err) throw err;
+//     console.log("Connected!");
+//     con.query("CREATE DATABASE Stock", function (err, result) {
+//       if (err) throw err;
+//       console.log("Database created");
+//     });
+// });
+
+exports.createTables = function(conData, callback){
+    var con = mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: "password",
+        database: "Stock"
     });
-});
+
+    var sql = "CREATE TABLE Items (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), quantity INT, date DATE)";
+    con.query(sql, function (err, result){
+        if (err) throw err;
+        console.log("Table created");
+    });
+};
