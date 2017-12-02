56d201ec96677657962f124f497e52ccde830bea
hillsdej <hillsdej@uni.coventry.ac.uk>
2017-12-02 23:02:51 +0000

    can now add an orders item

--- database.js
+++ database.js
@@ -40,19 +40,23 @@ exports.createTables = function(conData, callback){
 };
 
 //To check all items
-// selectAllOrders = function(conData, callback){
-//     var con =mysql.createConnection({
-//       host:"localhost",
-//       user:"root",
-//       password:"password",
-//       database:"SmartMart"
-//     });
+selectAllOrders = function(conData, callback){
+    var con =mysql.createConnection({
+      host:"localhost",
+      user:"root",
+      password:"password",
+      database:"SmartMart"
+    });
 
-//     var sql ="SELECT * FROM Orders;"
-//     con.query(sql, function(err, result){
-//       if (err) throw err;
-//       console.log(result);
-//     });
-// };
+    var milk = 'milk'
+    var sql = 'SELECT * FROM Order_Items;'
+    //var sql ="SELECT item_id FROM Stock ORDER BY item_id DESC LIMIT 1;"
+    //var sql ="ALTER TABLE Order_Items ADD PRIMARY KEY(order_id,item_id);"
+    var x;
+    con.query(sql, function(err, x){
+      if (err) throw err;
+      console.log(x);
+    });
+};
 
-// selectAllOrders();
+selectAllOrders();
--- orders.js
+++ orders.js
@@ -24,16 +24,53 @@ exports.add = function(conData, req, callback){
                     date: new Date()
                 };
 
-                data.query('INSERT INTO Orders SET ?', order, function(err, result){
-                    callback(err, order);
-                });
+                //get order id from
 
+                data.query('INSERT INTO Orders SET ?', order, function(err, result){
+                    if(err){
+                        callback(err);
+                        return;
+                    }
+                    else{
+                         addToOrderItems();
+                    }
+                    callback(err, order);                    
+                });
             }
             else{
                 let err = "incorrect name or password";
                 callback(err);
                 return;
             }
-        })
+        });
+
+        function addToOrderItems(){
+            var order_id;
+            data.query('SELECT order_id FROM Orders ORDER BY order_id DESC LIMIT 1;', function(err,order_id){
+                if(err){
+                    callback(err);
+                    return;
+                }
+                else{
+                    console.log(order_id);
+                    console.log(req.body['item_name'])
+                    var item_id;
+                    data.query('SELECT item_id FROM Stock WHERE item_name="' + req.body['item_name'] + '";', function(err,item_id){
+                        if (err) throw err;
+                        console.log(item_id)
+                        var itemInfo = {
+                            order_id : order_id[0].order_id,
+                            item_id : item_id[0].item_id,
+                            amount : req.body['amount']
+                        }
+                        
+                        data.query('INSERT INTO Order_Items SET ?', itemInfo, function(err, result){
+                            callback(err, itemInfo);
+                        });
+                    })
+                }
+            })
+        }
+
     });
 };
